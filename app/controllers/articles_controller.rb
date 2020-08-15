879bd971527c43e9664eb6ea3cd7dc10594f603b
Tim McCullough <tcm.coding@gmail.com>
2020-08-15 18:12:02 -0400

    Added Edit and Update Article code.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,11 +18,42 @@ class ArticlesController < ApplicationController
   end
   
   def new
+    @article = Article.new
+  end
 
+  def edit
+    # We need to identify the correct article to pass.
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    # We need to identify the correct article to pass.
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully."
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def create
-    render plain: params[:article]
+    #Use:  render plain: params[:article]  to preview passed params.
+
+    # We need to whitelist the fields we will be adding to the database:
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # Actually save the article to the DB. If successful, display message.
+   if  @article.save
+      flash[:notice] = "Article was created successfully."
+
+    #render plain: @article.inspect
+    #redirect_to article_path(@article)
+
+    #or better yet:
+    redirect_to @article
+   else
+    render 'new'
+   end
+
   end
-  
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,12 +5,4 @@ class PagesController < ApplicationController
   def about
   end
 
-  def new
-
-  end
-
-  def create
-    render plain: params[:article]
-  end
-  
 end
